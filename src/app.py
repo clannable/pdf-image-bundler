73b362fd47f268df8c237f6de120156ec3453911
Clark Annable <supergamebase@gmail.com>
2025-08-19 13:35:05 -0400

    Remove latex comment

--- src/app.py
+++ src/app.py
@@ -380,7 +380,7 @@ class App:
         with open(texSource, "r") as source:
             latexSource = source.read()
             if self.removeFirstPage.get():
-                preamble = "\\usepackage{atbegshi}% http://ctan.org/pkg/atbegshi\n\\AtBeginDocument{\\AtBeginShipoutNext{\\AtBeginShipoutDiscard}}\n"
+                preamble = "\\usepackage{atbegshi}\n\\AtBeginDocument{\\AtBeginShipoutNext{\\AtBeginShipoutDiscard}}\n"
             else:
                 preamble = ""
             latexSource = latexSource.replace("%PREAMBLE%", preamble)
